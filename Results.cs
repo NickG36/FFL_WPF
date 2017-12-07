1f0a2d87670930a7da2adda5d8130542c7c476df
NickG36 <Nick_Gliddon@hotmail.com>
2017-12-07 20:53:11 +0000

    Adding new file

---
+++ Results.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+/// <summary>
+/// Summary description for Results
+/// </summary>
+/// 
+namespace FFL_WPF
+{
+    public abstract class Results
+    {
+        abstract public bool doesFileExist();
+
+        abstract public void addResultsBlock(ResultsBlock results_block);
+            
+        abstract public List<CommonTypes.Result> getAllResults();
+
+        /// <summary>
+        /// A ResultsBlock represents one set of results (normally one week's worth)
+        /// </summary>
+        public class ResultsBlock
+        {
+            public ResultsBlock()
+            {
+                results = new List<CommonTypes.Result>();
+            }
+            public string week_description;
+            public List<CommonTypes.Result> results;
+        }
+        abstract public List<ResultsBlock> getAllResultsBlocks();
+
+    }
+}
